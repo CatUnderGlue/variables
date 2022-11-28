8309fca7254822bda92baf64a152d41c4656acd1
CatUnderGlue <warmonds1996@gmail.com>
2022-11-28 16:30:09 +0500

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         var dog = 8;
+        dog = dog + 4;
         var cat = 3.6;
+        cat = cat + 4;
         var paper = 763789;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
